e0da7597daacb4c09463ee48525dd7e4c600e430
Paul Cheney <pwcbyui.edu>
2020-01-21 11:49:37 -0700

    Updated Master Files

--- toggleMenu.js
+++ toggleMenu.js
@@ -1,5 +1,6 @@
 //Create a button and give it an ID of "hamburgerBtn"
 //Attach an ID of "primaryNav" to the UL of the unordered list that holds your links
+
 function toggleMenu() {
     document.getElementById("hamburgerBtn").classList.toggle("open");
     document.getElementById("primaryNav").classList.toggle("open");
